cfdf2ae8baf87837afd4bdcabc7fbb448192279f
Ted-star <kibuthuteddy@gmail.com>
2024-10-29 15:17:19 +0300

    wueeh

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserShield, faChartLine, faWallet } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  // Icons for Font Awesome
+  faUserShield = faUserShield;
+  faChartLine = faChartLine;
+  faWallet = faWallet;
+}
